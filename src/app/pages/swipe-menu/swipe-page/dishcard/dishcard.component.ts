9f8c41195493f2d8dbd16683886d0deefd4b41d3
Youri Saman <youri.saman10@gmail.com>
2019-10-09 13:30:21 +0200

    next/previous item added to card clicks

--- src/app/pages/swipe-menu/swipe-page/dishcard/dishcard.component.ts
+++ src/app/pages/swipe-menu/swipe-page/dishcard/dishcard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuItem } from 'src/app/core/classes/menu-item';
 
 @Component({
   selector: 'app-dishcard',
@@ -8,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class DishcardComponent implements OnInit {
 
   animationState: string;
+  @Input() item: MenuItem;
 
   constructor() { }
 

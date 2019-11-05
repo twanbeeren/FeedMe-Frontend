e60e263c4112b8cb3513315dbb0b5d9e2c41fcd6
twanmennen112 <twan.mennen@student.fontys.nl>
2019-11-05 11:14:50 +0100

    Add and remove item with increments of 1

--- src/app/core/classes/order.ts
+++ src/app/core/classes/order.ts
@@ -5,26 +5,35 @@ export class Order {
     id: string;
     tableNr: number;
     time: Date;
-    orderItems: { item: MenuItem, amount: number }[] = [];
+    orderItems: { item: MenuItem, amount: number }[];
     status: string;
 
-    constructor(table: number){
-        this.id = Guid.raw();
+    constructor(table: number) {
         this.tableNr = table;
         this.time = new Date();
+        this.id = Guid.raw();
+    }
+
+    removeItem(itemId: string) {
+      this.orderItems.forEach(orderItem => {
+        if (orderItem.item.id === itemId) {
+          orderItem.amount--;
+          if (orderItem.amount === 0) {
+            const index = this.orderItems.indexOf(orderItem);
+            this.orderItems.splice(index);
+          }
+          return;
+        }
+      });
     }
 
     addItem(itemToAdd: MenuItem) {
-        this.orderItems.push({ item: itemToAdd, amount: 1 });
-    }
-
-    removeItem(id: string) {
-        this.orderItems.forEach(orderItem => {
-            if (orderItem.item.id === id) {
-                const index = this.orderItems.indexOf(orderItem);
-                this.orderItems.splice(index);
-                return;
-            }
-        });
+      this.orderItems.forEach(orderItem => {
+        if (orderItem.item.id === itemToAdd.id){
+          orderItem.amount++;
+          return;
+        }
+      });
+      this.orderItems.push({ item: itemToAdd, amount: 1});
     }
 }

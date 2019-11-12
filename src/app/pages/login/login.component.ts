bac93bea376111816e4f4969c8e2167e519daa14
Paul <paulusman@live.nl>
2019-11-12 12:52:34 +0100

    Shows error

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,12 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
+  hasError = false;
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
     this.initForm();
@@ -27,13 +30,14 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.hasError = false;
     const email = this.loginForm.get('email').value;
     const password = this.loginForm.get('password').value;
 
     this.authService.emailLogin(email, password).then(x => {
-      console.log(x);
-    }).catch(error => {
-      console.log(error);
+      this.router.navigate(['/swipemenu']);
+    }).catch(() => {
+      this.hasError = true;
     });
   }
 

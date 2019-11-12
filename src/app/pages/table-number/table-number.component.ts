e6192f63d05a61044f0fff53c7cc4cda08cba5c5
BobvanVelzen <346501@student.fontys.nl>
2019-11-12 10:58:04 +0100

    database validation for password

--- src/app/pages/table-number/table-number.component.ts
+++ src/app/pages/table-number/table-number.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { TranslatorService } from 'src/app/core/services/translator.service';
+import { ValidateService } from 'src/app/core/services/validate.service';
 
 @Component({
   selector: 'app-table-number',
@@ -16,6 +17,7 @@ export class TableNumberComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
+    private validateService: ValidateService,
     private router: Router,
     private translator: TranslatorService,
     private snackbar: MatSnackBar) {}
@@ -26,12 +28,13 @@ export class TableNumberComponent implements OnInit {
 
   setTableNumber(tableFormValue) {
     let tableNr: number = parseInt(tableFormValue.tableNumber);
-    console.log(tableFormValue.tableNumber);
-    console.log(tableFormValue.password);
-    if (!isNaN(tableFormValue.tableNumber) && tableFormValue.password === 'admin') { // TEMP PASSWORD
-      this.orderService.newOrder(tableNr);
-      console.log("new order has been made");
-      this.router.navigateByUrl("/swipemenu");
+    if (!isNaN(tableFormValue.tableNumber)) { // Is the tableNumber actually a number?
+      this.validateService.validate(tableFormValue.password).subscribe(valid => { // Validate password with database
+        if (valid) {
+          this.orderService.setTableNumber(tableNr); // Set tableNumber for new orders
+          this.router.navigateByUrl("/swipemenu"); // Go to swipe menu if validated
+        } else this.showIncorrectSnackbar();
+      });
     } else this.showIncorrectSnackbar();
   }
 

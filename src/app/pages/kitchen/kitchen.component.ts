566f0f27fa1313c3d4be346a6831c05779ac990c
Beeren,Twan T.W.A <t.beeren@student.fontys.nl>
2019-12-03 15:02:49 +0100

    Tiles system

--- src/app/pages/kitchen/kitchen.component.ts
+++ src/app/pages/kitchen/kitchen.component.ts
@@ -3,6 +3,11 @@ import { Observable, of } from 'rxjs';
 import { KitchenService } from 'src/app/core/services/kitchen.service';
 import { Order } from 'src/app/core/classes/order';
 
+export class Tile {
+  color: string;
+  order: Order;
+}
+
 @Component({
   selector: 'app-kitchen',
   templateUrl: './kitchen.component.html',
@@ -10,12 +15,34 @@ import { Order } from 'src/app/core/classes/order';
 })
 export class KitchenComponent implements OnInit {
 
-  orders$: Observable<Order[]>;
-
   constructor(public kitchenService: KitchenService) { }
+
+  orders$: Observable<Order[]>;
+  tile: Tile;
+
+  // tiles: Tile[] = [
+  //   {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+  //   {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+  //   {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  //   {text: 'Four', cols: 1, rows: 1, color: 'lightpink'}
+  // ];
+
+  tiles: Tile[] = [];
 
   ngOnInit() {
     this.orders$ = this.kitchenService.getOrders();
+    this.setTiles();
+  }
+
+  setTiles() {
+    this.orders$.subscribe(orders => {
+      orders.forEach(order => {
+        this.tile = new Tile();
+        this.tile.color = 'lightblue';
+        this.tile.order = order;
+        this.tiles.push(this.tile);
+      });
+    });
   }
 
   setStatus(id: string, newStatus: string) {

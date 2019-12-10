fd86cfcb46d5501ec37ff81842892ffd6245febc
Paul <paulusman@live.nl>
2019-12-10 10:49:50 +0100

    Mock payment

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TicketService } from 'src/app/core/services/ticket.service';
 import { OrderService } from 'src/app/core/services/order.service';
 import { MenuItem } from 'src/app/core/classes/menu-item';
+import { PaymentService } from 'src/app/core/services/payment.service';
 
 @Component({
   selector: 'app-payment',
@@ -12,7 +13,10 @@ export class PaymentComponent implements OnInit {
 
   tableNrParam;
 
-  constructor(public ticketService: TicketService, public orderService: OrderService) { }
+  constructor(
+    public ticketService: TicketService,
+    public orderService: OrderService,
+    private paymentService: PaymentService) { }
 
   ngOnInit() {
     this.ticketService.tableNumber.subscribe(tableNr => this.tableNrParam = `{number: ${tableNr}}`);
@@ -23,5 +27,9 @@ export class PaymentComponent implements OnInit {
         menuItem.item = retrievedItem.data();
       }));
     }));
+  }
+
+  pay() {
+    this.paymentService.pay();
   }
 }
